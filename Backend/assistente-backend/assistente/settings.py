e233ef30393d5a9f8860bf910c069ad90ba2e175
Silas Torres Rocha <43487074+SilasTorresRocha@users.noreply.github.com>
2025-06-15 17:32:09 -0300

    Backend 1.0.4
    Add usuario no banco

--- Backend/assistente-backend/assistente/settings.py
+++ Backend/assistente-backend/assistente/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +83,7 @@ DATABASES = {
         'NAME': 'bd',
         'USER': 'postgres',
         'PASSWORD': 'senha',
-        'HOST': os.environ.get('DJANGO_DB_HOST', 'localhost'),  
+        'HOST': 'host.docker.internal',  
         'PORT': '5432',
     }
 }
